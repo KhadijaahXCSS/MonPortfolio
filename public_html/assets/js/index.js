1d458f9ab19a30e694fee5a0280c2fc0c5f3d2f1
KhadijaahXCSS <nangela837@gmail.com>
2025-05-08 11:58:18 +0000

    petit reglage

--- public_html/assets/js/index.js
+++ public_html/assets/js/index.js
@@ -1,6 +1,6 @@
 /*!
 =========================================================
-* JohnDoe Landing page
+* Khady Landing page
 =========================================================
 
 * Copyright: 2019 DevCRUD (https://devcrud.com)
